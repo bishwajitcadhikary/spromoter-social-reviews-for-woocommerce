101edb8e2b95e4bf7de5cf49f001c5bbab80e054
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2024-01-22 21:45:40 +0600

    Initial Commit

---
+++ spromoter.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * SPromoter Social Reviews for WooCommerce
+ *
+ * @package           SPromoter
+ * @author            SPromoter Social Reviews for WooCommerce
+ * @copyright         2024 WovoSoft
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       SPromoter Social Reviews for WooCommerce
+ * Plugin URI:        https://reviews.spromoter.com/integrations/woocommerce
+ * Description:       SPromoter Social Reviews for WooCommerce helps you to collect and display reviews from your customers on your WooCommerce store.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            SPromoter
+ * Author URI:        https://spromoter.com
+ * Text Domain:       spromoter-social-reviews-for-woocommerce
+ * Domain Path:       /languages
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
