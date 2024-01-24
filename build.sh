8e8f317a50a5987829b6a79a9f57f9894eed4914
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2024-01-24 23:50:01 +0600

    Version increased to 1.0.10

--- build.sh
+++ build.sh
@@ -12,14 +12,16 @@ new_version=$(echo "$current_version" | awk -F. '{print $1"."$2"."$3 + 1}')
 # Update the version in the PHP file
 sed -i "s/Version:           $current_version/Version:           $new_version/" "$php_file"
 
-# Display the updated version
-echo "Version increased to $new_version"
+# Display the updated version in green
+echo -e "\e[32mVersion increased to $new_version\e[0m"
 
-echo "Commiting changes to git & pushing to remote"
+# Display git-related messages in cyan
+echo -e "\e[36mCommitting changes to git & pushing to remote\e[0m"
 git add .
 git commit -m "Version increased to $new_version"
 git push origin main
 
-echo "Creating a new tag"
+# Display tag-related messages in yellow
+echo -e "\e[33mCreating a new tag\e[0m"
 git tag -a "$new_version" -m "Version $new_version"
 git push origin "$new_version"
