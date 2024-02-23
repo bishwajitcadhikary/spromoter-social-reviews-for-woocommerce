4aa0c82a1b3197532ca1f1a95bf640275c87aed8
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2024-02-24 04:12:20 +0600

    Version increased to 1.1.1

--- build.sh
+++ build.sh
@@ -4,24 +4,24 @@
 php_file="spromoter.php"
 
 # Extract the current version from the PHP file
-current_version=$(grep -oP "Version:           \K[0-9.]+" "$php_file")
+current_version=$(grep -oE "Version:[[:space:]]+[0-9.]+" "$php_file" | awk '{print $2}')
 
 # Increase the version number (assuming a standard version format like x.y.z)
-new_version=$(echo "$current_version" | awk -F. '{print $1"."$2"."$3 + 1}')
+new_version=$(echo "$current_version" | awk -F '.' '{print $1"."$2"."$3 + 1}')
 
 # Update the version in the PHP file
-sed -i "s/Version:           $current_version/Version:           $new_version/" "$php_file"
+sed -i '' "s/Version:[[:space:]]*$current_version/Version: $new_version/" "$php_file"
 
 # Display the updated version in green
-echo -e "\e[32mVersion increased to $new_version\e[0m"
+echo -e "\033[0;32mVersion increased to $new_version\033[0m"
 
 # Display git-related messages in cyan
-echo -e "\e[36mCommitting changes to git & pushing to remote\e[0m"
+echo -e "\033[0;36mCommitting changes to git & pushing to remote\033[0m"
 git add .
 git commit -m "Version increased to $new_version"
 git push origin main
 
 # Display tag-related messages in yellow
-echo -e "\e[33mCreating a new tag\e[0m"
+echo -e "\033[0;33mCreating a new tag\033[0m"
 git tag -a "$new_version" -m "Version $new_version"
 git push origin "$new_version"
